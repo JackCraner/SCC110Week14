4a14b257787f3519a065f684dff91933fb45648f
Dickens <dickence@egb070000013.lancs.local>
2020-02-07 14:27:30 +0000

    message

---
+++ ANiceGame/CathDriver.java
@@ -0,0 +1,11 @@
+public class CathDriver
+{
+    public static void main(String[] args)
+    {
+        GameArena map = new GameArena(500,500);
+        Plane allie = new Plane();
+        map.addPlane(allie);
+
+    }
+
+}
---
+++ ANiceGame/Plane.java
@@ -0,0 +1,13 @@
+public class Plane
+{
+    public Plane()
+    {
+        Rectangle head = new Rectangle(100,100,50,50,"Red");
+    }
+    public static void main(String[] args)
+    {
+        
+        
+    }
+
+}
